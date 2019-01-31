90b20985e6ae5c3d763746b65f189beabc8a145e
Crispin Wellington <retrogradeorbit@gmail.com>
2019-02-01 00:01:28 +0800

    complete controls

--- src/otoch/controls.cljs
+++ src/otoch/controls.cljs
@@ -4,24 +4,39 @@
             [infinitelives.utils.vec2 :as vec2]
             ))
 
+(defn start-game? []
+  (or
+   (e/any-pressed?)
+   (gp/button-pressed? :a)
+   (gp/button-pressed? :b)
+   (gp/button-pressed? :x)
+   (gp/button-pressed? :y)
+   (gp/button-pressed? :start)))
+
 (defn jump-button-pressed? []
   (or
    (e/is-pressed? :z)
    (e/is-pressed? :space)
-   (gp/button-pressed? 0 :x)))
+   (e/is-pressed? :comma)
+   (gp/button-pressed? 0 :x)
+   (gp/button-pressed? 0 :y)))
 
 (defn throw-rune-pressed? []
   (or
    (e/is-pressed? :x)
-   (gp/button-pressed? 0 :y)))
+   (e/is-pressed? :.)
+   (gp/button-pressed? 0 :a)
+   (gp/button-pressed? 0 :b)))
 
 (defn get-joy-vec []
-  (vec2/vec2 (or (gp/axis 0)
-                 (cond (e/is-pressed? :left) -1
-                       (e/is-pressed? :right) 1
-                       :default 0) )
-             (or (gp/axis 1)
-                 (cond (e/is-pressed? :up) -1
-                       (e/is-pressed? :down) 1
-                       :default 0)
-                 )))
+  (vec2/vec2 (cond (e/is-pressed? :left) -1
+                   (e/is-pressed? :a) -1
+                   (e/is-pressed? :right) 1
+                   (e/is-pressed? :d) 1
+                   :default (or (gp/axis 0) 0))
+             (cond (e/is-pressed? :up) -1
+                   (e/is-pressed? :w) -1
+                   (e/is-pressed? :down) 1
+                   (e/is-pressed? :s) 1
+                   :default (or (gp/axis 1) 0))
+                 ))
